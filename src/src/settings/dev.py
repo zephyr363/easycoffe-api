65531c4634cd74f9cc72cf0d2b4b153de682e971
fluxzio <fluxiodev@gmail.com>
2025-10-15 22:32:23 +0500

    initial

---
+++ src/src/settings/dev.py
@@ -0,0 +1,11 @@
+from .base import *  # noqa
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
